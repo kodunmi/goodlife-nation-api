bb185824bea149a9549714f417e4b20f745a131c
kodunmi <53086177+kodunmi@users.noreply.github.com>
2022-03-21 11:32:58 +0100

    heroku port

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,6 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
-  await app.listen(3030);
+  await app.listen(process.env.PORT || 3030);
 }
 bootstrap();
